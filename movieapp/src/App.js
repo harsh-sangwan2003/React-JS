037be0afad4524dad08606c8ed1510b53a92e569
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-20 23:33:10 +0530

    added

---
+++ movieapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Banner from './components/Banner';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+
+    <>
+      <NavBar />
+      <Banner />
+      <MovieList/>
+    </>
+
+  );
+}
+
+export default App;
